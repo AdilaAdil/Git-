45216c47cf7d1fff66d87273cc394464e454e29d
ibadet1002 <57850215+ibadet1002@users.noreply.github.com>
2020-02-29 12:31:18 -0500

    Update Login.java

--- src/test/Login.java
+++ src/test/Login.java
@@ -3,5 +3,8 @@ package test;
 public class Login {
     public static void main(String[] args) {
         System.out.println(" Hello Asiye! == Hello Git!");
+        
+        String name = "Asiye";
+        System.out.println(name);
     }
 }
